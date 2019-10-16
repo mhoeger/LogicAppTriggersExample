19c5a71d6c51430a15da44d243c38b3242f43ab3
Marie Hoeger <marie.hoeger@gmail.com>
2019-10-16 12:59:20 -0700

    return 201 on subscribe

--- LogicAppTriggers/Controllers/WebhookTriggerController.cs
+++ LogicAppTriggers/Controllers/WebhookTriggerController.cs
@@ -22,7 +22,7 @@ namespace LogicAppTriggers.Controllers
         public HttpResponseMessage Subscribe([FromBody] string callbackUrl)
         {
             subscriptions.Add(callbackUrl);
-            HttpResponseMessage responseMessage = Request.CreateResponse();
+            HttpResponseMessage responseMessage = Request.CreateResponse(HttpStatusCode.Accepted);
             responseMessage.Headers.Add("location", String.Format("{0}://{1}/api/webhooktrigger/unsubscribe", Request.RequestUri.Scheme, Request.RequestUri.Host));  //Where the engine will poll to check status
             return responseMessage;
         }
