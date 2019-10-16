9d204e5aeebb59d7d1723292344ec08f4971d72d
Marie Hoeger <marie.hoeger@gmail.com>
2019-10-16 14:29:00 -0700

    trying this out

--- LogicAppTriggers/Controllers/PollTriggerController.cs
+++ LogicAppTriggers/Controllers/PollTriggerController.cs
@@ -45,7 +45,18 @@ namespace LogicAppTriggers.Controllers
                     return GenerateAsyncResponse(HttpStatusCode.Accepted, triggerState, "15");
                 }
             }
-
+        }
+                    
+        /// <summary>
+        /// Fire all triggers - do a GET to this API to fire all triggers subscribed
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("/trigger/api/polltrigger")]
+        public HttpResponseMessage GetTrigger()
+        {
+            HttpResponseMessage responseMessage = Request.CreateResponse(HttpStatusCode.Accepted);
+            responseMessage.Content = new StringContent("Triggered here!");
+            return responseMessage;
         }
 
         private HttpResponseMessage GenerateAsyncResponse(HttpStatusCode code, string triggerState, string retryAfter)
